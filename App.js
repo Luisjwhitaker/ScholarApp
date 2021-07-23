012d8430062af3349cd5b03d8e6a09eec9cc665b
user.email <luisjwhitaker@gmail.com>
2021-07-23 21:59:09 +0800

    got app to display text and button

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert} from 'react-native';
 import Home from './Screens/home'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Home/>
+      <Button
+        onPress={() => Alert.alert('Cannot Log in at this time')}
+        title="LogIn"
+        color="#f194ff"
+        accessibilityLabel="press this button to Log In "
+      />
+
     </View>
   );
 }
--- Screens/home.js
+++ Screens/home.js
@@ -3,18 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 function Home(){
   return(
-    <Card style = {style.cardStyle}
-    <Text>Hello From Home Screen</Text>
-  )
+    <Text>Welcom Scholars!</Text>
+  );
 }
 
-const styles = StyleSheet.create({
-  cardStyle: {
-    flex: 1,
-    backgroundColor: '#ffe6d9',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
 
 export default Home
